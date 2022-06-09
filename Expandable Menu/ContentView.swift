4253bf34c558eacf27e6a1a3021f1b1a3c6dbb8d
Mohammad Komeili <momeksdev@gmail.com>
2022-06-09 19:13:40 +0430

    Initial Commit

---
+++ Expandable Menu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Expandable Menu
+//
+//  Created by Momeks on 6/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Expandable Menu/Expandable_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expandable_MenuApp.swift
+//  Expandable Menu
+//
+//  Created by Momeks on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Expandable_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
